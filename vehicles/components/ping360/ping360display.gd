e20f717988edca497c3d2bd90fa40cd8a40486ce
Willian Galvani <williangalvani@gmail.com>
2024-03-12 13:28:07 -0300

    fix for linux

--- vehicles/components/ping360/ping360display.gd
+++ vehicles/components/ping360/ping360display.gd
@@ -1,7 +1,7 @@
 extends Control
 
 var ping = null
-var previous_frame: Texture = null
+var previous_frame: Image = null
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -11,12 +11,11 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	self.previous_frame = self.get_viewport().get_texture().get_image()
 	if self.previous_frame:
-		self.material.set_shader_parameter("previous_image", self.previous_frame)
-	self.previous_frame = self.get_viewport().get_texture()
+		self.material.set_shader_parameter("previous_image", ImageTexture.new().create_from_image(self.previous_frame))
 	self.material.set_shader_parameter("depth_map", self.ping.image)
 	self.material.set_shader_parameter("camera_angle", self.ping.angle)
 	self.material.set_shader_parameter("size", self.ping.size.x)
 	self.material.set_shader_parameter("fov", self.ping.fov)
-
-
+	
